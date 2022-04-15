3ab861f1cd13187ac9f389e58179bae5198829b0
Sonette Yao <sonetteyao@gmail.com>
2022-04-16 00:43:39 +0800

    add user prompt to determine how many squares

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.getElementById('grid-container');
-let square = document.createElement('div');
-square.innerHTML = "here's one";
-container.appendChild(square);
 
-square.style.backgroundColor = 'white';
+let howMany = prompt('How many squares?');
+
+for (let i = 0; i < howMany; i++) {
+    let square = document.createElement('div');
+    square.innerHTML = "here's one";
+    container.appendChild(square);
+    square.style.backgroundColor = 'white';
+}
