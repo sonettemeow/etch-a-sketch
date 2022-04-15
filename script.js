91cfabbb2fd190bc237c44153d3b33f4692242fb
Sonette Yao <sonetteyao@gmail.com>
2022-04-16 00:25:35 +0800

    Initial html, css, js

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('grid-container');
+let square = document.createElement('div');
+square.innerHTML = "here's one";
+container.appendChild(square);
+
+square.style.backgroundColor = 'white';
