f8c9307e016ced7f6d1c9de947f5cb58cb5194ca
Sonette Yao <sonetteyao@gmail.com>
2022-04-18 21:31:53 +0800

    one row

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 //let howMany = prompt('How many squares?');
 
 const container = document.querySelector('.gridbox');
+const row = document.querySelector('.row');
 
 const numbah = 5;
 
 for (let i = 0; i < numbah; i++) {
-  const div = document.createElement('div');
-  div.style.border = '2px solid blue';
-  div.style.margin = '1px';
-  div.style.width = '100%';
-  container.appendChild(div);
+    const div = document.createElement('div');
+    div.style.border = '2px solid blue';
+    div.style.margin = '1px';
+    div.style.width = '100%';
+    row.appendChild(div);
 }
+const brk = document.createElement('div');
+brk.style.border = '1px solid white';
+brk.style.margin = '1px';
+brk.style.height = '0%';
+brk.style.width = '100%';
+container.appendChild(brk);
 
 // for (let rows = 0; rows < howMany; rows++) {
 //     for (let columns = 0; columns < howMany; columns++) {
