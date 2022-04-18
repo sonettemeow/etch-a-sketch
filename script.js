345153b988d0c4b9411856fca0b5717e3e026398
Sonette Yao <sonetteyao@gmail.com>
2022-04-18 16:27:31 +0800

    overhaul

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const container = document.getElementById('grid-container');
+//let howMany = prompt('How many squares?');
 
-let howMany = prompt('How many squares?');
+const container = document.querySelector('.gridbox');
 
-for (let i = 0; i < howMany; i++) {
-    let square = document.createElement('div');
-    square.innerHTML = "here's one";
-    container.appendChild(square);
-    square.style.backgroundColor = 'white';
+const numbah = 5;
+
+for (let i = 0; i < numbah; i++) {
+  const div = document.createElement('div');
+  div.style.border = '2px solid blue';
+  div.style.margin = '1px';
+  div.style.width = '100%';
+  container.appendChild(div);
 }
+
+// for (let rows = 0; rows < howMany; rows++) {
+//     for (let columns = 0; columns < howMany; columns++) {
+//         let square = document.createElement('div');
+//         square.innerHTML = "here's one";
+//         container.appendChild(square);
+//         square.style.backgroundColor = 'white';
+//         square.style.display = 'inline-block';
+//         //square.style.display = 'flex';
+//         //square.style.flexWrap = 'wrap';
+//     }
+// }
