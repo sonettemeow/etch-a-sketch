1c496c0a5791b9825814a7369f4af4d04123e2d2
Sonette Yao <sonetteyao@gmail.com>
2022-04-19 16:39:22 +0800

    fix prompt and reload; add light border to grid

--- script.js
+++ script.js
@@ -1,73 +1,85 @@
-//let size = prompt('How many squares?');
+function loadPage() {
+    let size = prompt('How many boxes? Will be applied to both rows and columns. Enjoy!');
 
-const container = document.querySelector('.gridbox');
-const clearBtn = document.getElementById('clear').addEventListener('click', clearGrid);
-const rgbBtn = document.getElementById('rgb').addEventListener('click', rgb);
-const bwgBtn = document.getElementById('bwg').addEventListener('click', bwg);
-const pinkBtn = document.getElementById('pink').addEventListener('click', pink);
-const buttons = document.querySelector('buttons');
-
-function createGrid(size) {
-    for (let i = 0; i < size; i++) {
-        const divRow = document.createElement('div');
-        divRow.style.display = 'flex';
-        divRow.style.flexGrow = '1';
-        divRow.style.boxSizing = 'border-box';
-        container.appendChild(divRow);
-
-        for (let j = 0; j < size; j++) { 
-            const cell = document.createElement('div');  
-            cell.style.border = '1px solid #101010';
-            cell.style.backgroundColor = 'white';
-            cell.style.margin = '0px';
-            cell.style.width = '100%';
-            cell.style.boxSizing = 'border-box';
-            divRow.appendChild(cell).classList.add('block');
+    if (size) {
+        if (size > 100) {
+            size = prompt('Please enter a number from 1-100.');
+        }
+        
+        const container = document.querySelector('.gridbox');
+        const clearBtn = document.getElementById('clear').addEventListener('click', clearGrid);
+        const rgbBtn = document.getElementById('rgb').addEventListener('click', rgb);
+        const bwgBtn = document.getElementById('bwg').addEventListener('click', bwg);
+        const pinkBtn = document.getElementById('pink').addEventListener('click', pink);
+        const buttons = document.querySelector('buttons');
+        
+        function createGrid(size) {
+            for (let i = 0; i < size; i++) {
+                const divRow = document.createElement('div');
+                divRow.style.display = 'flex';
+                divRow.style.flexGrow = '1';
+                divRow.style.boxSizing = 'border-box';
+                container.appendChild(divRow);
+        
+                for (let j = 0; j < size; j++) { 
+                    const cell = document.createElement('div');  
+                    cell.style.border = '1px solid #101010';
+                    cell.style.backgroundColor = 'white';
+                    cell.style.margin = '0px';
+                    cell.style.width = '100%';
+                    cell.style.boxSizing = 'border-box';
+                    divRow.appendChild(cell).classList.add('block');
+                    }
+        
+                const brk = document.createElement('div');
+                brk.style.height = '0%';
+                brk.style.width = '100%';
+                brk.style.boxSizing = 'border-box';
+                container.appendChild(brk);
             }
-
-        const brk = document.createElement('div');
-        brk.style.height = '0%';
-        brk.style.width = '100%';
-        brk.style.boxSizing = 'border-box';
-        container.appendChild(brk);
+        }
+        
+        createGrid(size);
+        
+        function clearGrid(e) {
+            const blocks = container.querySelectorAll('.block');
+            blocks.forEach(block => block.style.backgroundColor = 'white');
+            blocks.forEach(block => block.addEventListener('mouseover', () => {
+                block.style.backgroundColor = 'white';
+            }))
+        }
+        
+        function bwg(e) {
+            const blocks = container.querySelectorAll('.block');
+            blocks.forEach(block => block.style.backgroundColor = 'white');
+            blocks.forEach(block => block.addEventListener('mouseover', () => {
+                let ran = Math.floor(Math.random() * 256);
+                const gray = `rgb(${ran}, ${ran}, ${ran})`;
+                block.style.backgroundColor = gray;
+            }))
+        }
+        
+        function rgb(e) {
+            const blocks = container.querySelectorAll('.block');
+            blocks.forEach(block => block.style.backgroundColor = 'white');
+            blocks.forEach(block => block.addEventListener('mouseover', () => {
+                let R = Math.floor(Math.random() * 255);
+                let G = Math.floor(Math.random() * 255);
+                let B = Math.floor(Math.random() * 255);
+                const RGB = `rgb(${R}, ${G}, ${B})`;
+                block.style.backgroundColor = RGB;
+            }))
+        }
+        
+        function pink(e) {
+            const blocks = container.querySelectorAll('.block');
+            blocks.forEach(block => block.style.backgroundColor = 'white');
+            blocks.forEach(block => block.addEventListener('mouseover', () => {
+                block.style.backgroundColor = 'rgb(243, 58, 106)';
+            }))
+        }
+    } else {
+        loadPage();
     }
 }
-createGrid(30);
-
-function clearGrid(e) {
-    const blocks = container.querySelectorAll('.block');
-    blocks.forEach(block => block.style.backgroundColor = 'white');
-    blocks.forEach(block => block.addEventListener('mouseover', () => {
-        block.style.backgroundColor = 'white';
-    }))
-}
-
-function bwg(e) {
-    const blocks = container.querySelectorAll('.block');
-    blocks.forEach(block => block.style.backgroundColor = 'white');
-    blocks.forEach(block => block.addEventListener('mouseover', () => {
-        let ran = Math.floor(Math.random() * 256);
-        const gray = `rgb(${ran}, ${ran}, ${ran})`;
-        block.style.backgroundColor = gray;
-    }))
-}
-
-function rgb(e) {
-    const blocks = container.querySelectorAll('.block');
-    blocks.forEach(block => block.style.backgroundColor = 'white');
-    blocks.forEach(block => block.addEventListener('mouseover', () => {
-        let R = Math.floor(Math.random() * 255);
-        let G = Math.floor(Math.random() * 255);
-        let B = Math.floor(Math.random() * 255);
-        const RGB = `rgb(${R}, ${G}, ${B})`;
-        block.style.backgroundColor = RGB;
-    }))
-}
-
-function pink(e) {
-    const blocks = container.querySelectorAll('.block');
-    blocks.forEach(block => block.style.backgroundColor = 'white');
-    blocks.forEach(block => block.addEventListener('mouseover', () => {
-        block.style.backgroundColor = 'rgb(243, 58, 106)';
-    }))
-}
+loadPage();
